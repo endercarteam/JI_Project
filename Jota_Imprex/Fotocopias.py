3a8aed2094f5ed548b977eec07a528d1c81a14d9
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2022-11-24 03:31:26 -0500

    Update Fotocopias.py

--- Jota_Imprex/Fotocopias.py
+++ Jota_Imprex/Fotocopias.py
@@ -1,1 +1,26 @@
-
+class Fotocopias:
+    def __init__(self, Cantidad=int, Tipo=str, Precio=int) -> None:
+        self.Cantidad = Cantidad
+        self.Tipo=Tipo
+        self.Precio = Precio
+    @classmethod
+    def BlancoyNegro(self, Cantidad):
+        self.Cantidad = Cantidad
+        self.Tipo= "Blanco y Negro"
+        self.Precio = 200
+        ToT = self.Cantidad * self.Precio
+        return ToT
+    @classmethod
+    def Color(self, Cantidad):
+        self.Cantidad = Cantidad
+        self.Tipo= "Color"
+        self.Precio = 400
+        ToT = self.Cantidad * self.Precio
+        return ToT
+    @classmethod
+    def Pormayor(self, Cantidad):
+        self.Cantidad = Cantidad
+        self.Tipo= "Fotocopias al por mayor"
+        self.Precio = 80
+        ToT = self.Cantidad * self.Precio
+        return ToT    
