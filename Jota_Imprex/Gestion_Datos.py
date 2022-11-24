b8fb0f5fa45787ed6385d4768d2b7771a0dccbdc
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2022-11-24 01:19:57 -0500

    Update Gestion_Datos.py

--- Jota_Imprex/Gestion_Datos.py
+++ Jota_Imprex/Gestion_Datos.py
@@ -2,12 +2,16 @@ import json
 import abc
 from Fotos import Fotos
 from Servicio_Fotografico import Servicio_Fotografico
+from Impresion import Impresion
 Fotos10x15 = Fotos.Fotos10x15
 Fotos13x18 = Fotos.Fotos13x18
 Fotos20x25 = Fotos.Fotos20x25
 Fotos8 = Servicio_Fotografico.Fotos8
 Fotos12 = Servicio_Fotografico.Fotos12
 Fotos24 = Servicio_Fotografico.Fotos24
+BlancoyNegro = Impresion.BlancoyNegro
+Color = Impresion.Color
+Pormayor = Impresion.Pormayor
 class Gestion_Datos(abc.ABC):
     @abc.abstractclassmethod
     def add_Data(self)->None:
