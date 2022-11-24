dccc12f70b6d91e1315f237e81362326e5e3e554
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2022-11-23 20:33:40 -0500

    Update Gestion_Datos.py

--- Jota_Imprex/Gestion_Datos.py
+++ Jota_Imprex/Gestion_Datos.py
@@ -1,1 +1,15 @@
+import json
+import abc
+class Gestion_Datos(abc.ABC):
+    @abc.abstractclassmethod
+    def actualizar(self)->None:
+        ...
 
+
+class Pendientes(Gestion_Datos):
+    def actualizar(self)-> None:
+        pass
+
+class Completados(Gestion_Datos):
+    def actualizar(self)-> None:
+        pass           
