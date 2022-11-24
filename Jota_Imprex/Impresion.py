5bd67f7ea56eddd57429e85f2d8fa081167bbf59
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2022-11-24 01:16:33 -0500

    Update Impresion.py

--- Jota_Imprex/Impresion.py
+++ Jota_Imprex/Impresion.py
@@ -1,1 +1,26 @@
+class Impresion:
+    def __init__(self, Cantidad=int, Tipo=str, Precio=int):
+        self.Cantidad = Cantidad
+        self.Tipo=str
+        self.Precio = int
 
+    def BlancoyNegro(self, Cantidad):
+        self.Cantidad = Cantidad
+        self.Tipo= "Blanco y Negro"
+        self.Precio = 600
+        ToT = self.Cantidad * self.Precio
+        return ToT
+
+    def Color(self, Cantidad):
+        self.Cantidad = Cantidad
+        self.Tipo= "Color"
+        self.Precio = 1200
+        ToT = self.Cantidad * self.Precio
+        return ToT
+
+    def Pormayor(self, Cantidad):
+        self.Cantidad = Cantidad
+        self.Tipo= "Fotos al por mayor"
+        self.Precio = 80
+        ToT = self.Cantidad * self.Precio
+        return ToT
