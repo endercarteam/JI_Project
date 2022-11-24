fb8a14577c794d4904b0545d7164e2f163ae67cb
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2022-11-24 01:32:34 -0500

    Create Edicion.py

---
+++ Jota_Imprex/Edicion.py
@@ -0,0 +1,19 @@
+class Edicion:
+    def __init__(self, Tipo:str, Cantidad:int, Precio:int):
+        self.Tipo = Tipo
+        self.Cantidad = Cantidad
+        self.Precio = Precio
+
+    def EN(self, Cantidad):
+        self.Tipo = "Edicion Normal"
+        self.Cantidad = Cantidad
+        self.Precio = 8000
+        Tot = self.Cantidad * self.Precio
+        return Tot
+
+    def Reparacion(self, Cantidad):
+        self.Tipo = "Reparacion de Fotos"
+        self.Cantidad = Cantidad
+        self.Precio = 12000
+        Tot = self.Cantidad * self.Precio
+        return Tot    
