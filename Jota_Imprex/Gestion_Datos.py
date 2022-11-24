52772a0a71666ec965338f5d407b3df481f20922
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2022-11-24 00:06:32 -0500

    Update Gestion_Datos.py

--- Jota_Imprex/Gestion_Datos.py
+++ Jota_Imprex/Gestion_Datos.py
@@ -12,8 +12,15 @@ class Gestion_Datos(abc.ABC):
 
 class Pendientes(Gestion_Datos):
     def add_Data(self)-> None:
+        print("Digite el nombre del cliente")
+        nombre = input()
+        print("escoja a continuacion la categoria:\n1.Fotos\n2.Impresion\n3.Fotocopia\n4.Escaneo\n5.Hoja de vida\n6.Edicion de fotos\n7.Reparacion de fotos")
+        Categoria = input()
+        if Categoria == 1:
+            Cat = "Fotos"
+            print("seleccione una de las siguientes opciones:\n1. Foto 10x15cm")
 
-        Pendiente = {}
+        Pendiente = {"nombre": nombre,"Categoria":Cat}
 
     def show_Data(self)->None:
         ...     
@@ -23,4 +30,4 @@ class Completados(Gestion_Datos):
         pass 
 
     def show_Data(self)->None:
-        ...     
+        ...           
