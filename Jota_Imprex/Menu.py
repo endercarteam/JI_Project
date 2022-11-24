2d5e3ed550b1f07d1a7fd4a5a0a30e90191c0190
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2022-11-23 20:34:05 -0500

    Update Menu.py

--- Jota_Imprex/Menu.py
+++ Jota_Imprex/Menu.py
@@ -1,1 +1,9 @@
+from Gestion_Datos import Pendientes, Completados
+i=0
+print("**********************")
+print("******Bienvenido******") 
+print("**********************")
 
+while(i==0):
+   print("¿que desea hacer?\n1. digitar nuevo pedido\n2.")
+   i=1
