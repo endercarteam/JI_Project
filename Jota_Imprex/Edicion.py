33e48c76d285f47a47744b8fd09c401720eed34e
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2022-11-24 03:00:15 -0500

    Update Edicion.py

--- Jota_Imprex/Edicion.py
+++ Jota_Imprex/Edicion.py
@@ -3,17 +3,17 @@ class Edicion:
         self.Tipo = Tipo
         self.Cantidad = Cantidad
         self.Precio = Precio
-
+    @classmethod
     def EN(self, Cantidad):
         self.Tipo = "Edicion Normal"
         self.Cantidad = Cantidad
         self.Precio = 8000
         Tot = self.Cantidad * self.Precio
         return Tot
-
+    @classmethod
     def Reparacion(self, Cantidad):
         self.Tipo = "Reparacion de Fotos"
         self.Cantidad = Cantidad
         self.Precio = 12000
         Tot = self.Cantidad * self.Precio
-        return Tot    
+        return Tot        
