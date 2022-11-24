fe8e5324d8c247201cee31dced124032e367c599
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2022-11-24 00:59:56 -0500

    Update Servicio_Fotografico.py

--- Jota_Imprex/Servicio_Fotografico.py
+++ Jota_Imprex/Servicio_Fotografico.py
@@ -1,1 +1,19 @@
+class Servicio_Fotografico:
+    def __init__(self, Cantidad=int, Precio=int):
+        self.Cantidad = Cantidad
+        self.Precio = Precio
 
+    def Fotos8(self):
+        self.Cantidad = 8
+        self.Precio = 55000
+        return self.Precio
+
+    def Fotos12(self):
+        self.Cantidad = 12
+        self.Precio = 80000
+        return self.Precio 
+
+    def Fotos24(self):       
+        self.Cantidad = 24
+        self.Precio = 125000
+        return self.Precio
