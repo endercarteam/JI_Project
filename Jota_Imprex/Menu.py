f800f2f14a3676f107db0f92124ee4a239478289
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2022-11-23 21:27:25 -0500

    Update Menu.py

--- Jota_Imprex/Menu.py
+++ Jota_Imprex/Menu.py
@@ -1,9 +1,10 @@
 from Gestion_Datos import Pendientes, Completados
 i=0
+h=0
 print("**********************")
 print("******Bienvenido******") 
 print("**********************")
 
 while(i==0):
-   print("¿que desea hacer?\n1. digitar nuevo pedido\n2.")
-   i=1
+   print("¿que desea hacer?\n1. digitar nuevo pedido\n2. marcar pedido como competado\n3.salir")
+   op1 = int(input())
