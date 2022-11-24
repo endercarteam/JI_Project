343513d961d925daf6c5cee6f2b8beec023f2c33
Carlos Herrera <91082864+endercarteam@users.noreply.github.com>
2022-11-24 03:03:12 -0500

    Update Gestion_Datos.py

--- Jota_Imprex/Gestion_Datos.py
+++ Jota_Imprex/Gestion_Datos.py
@@ -4,17 +4,8 @@ from Fotos import Fotos
 from Servicio_Fotografico import Servicio_Fotografico
 from Impresion import Impresion
 from Edicion import Edicion
-Fotos10x15 = Fotos.Fotos10x15
-Fotos13x18 = Fotos.Fotos13x18
-Fotos20x25 = Fotos.Fotos20x25
-Fotos8 = Servicio_Fotografico.Fotos8
-Fotos12 = Servicio_Fotografico.Fotos12
-Fotos24 = Servicio_Fotografico.Fotos24
-BlancoyNegro = Impresion.BlancoyNegro
-Color = Impresion.Color
-Pormayor = Impresion.Pormayor
-EN = Edicion.EN
-Reparacion = Edicion.Reparacion
+
+
 class Gestion_Datos(abc.ABC):
     @abc.abstractclassmethod
     def add_Data(self)->None:
@@ -28,14 +19,58 @@ class Gestion_Datos(abc.ABC):
 class Pendientes(Gestion_Datos):
     def add_Data(self)-> None:
         print("Digite el nombre del cliente")
-        nombre = input()
+        nombre = str(input())
         print("escoja a continuacion la categoria:\n1.Impresion de Fotos\n2. Servicio Fotografico\n3.Impresion\n4.Fotocopia\n5.Escaneo\n6.Hoja de vida\n7.Edicion de fotos\n8s.Reparacion de fotos")
-        Categoria = input()
+        Categoria = int(input())
         if Categoria == 1:
-            Cat = "Fotos"
-            print("seleccione una de las siguientes opciones:\n1. Foto 10x15cm")
+            Cat = str("Fotos")
+            print("seleccione una de las siguientes opciones:\n1. Foto 10x15cm\n2. Fotos 13x18\n3. Fotos 20x25")
+            F = int(input())
+            if F == 1:
+                Tipo: str("Tamaño 10x15")
+                print("Digite la cantidad de Fotos que requiere")
+                Cant = int(input())
+                Price = Fotos.Fotos10x15(Cant)
+                print (F"El precio a pagar es: ", Price)
+            else: 
+                if F ==2:
+                    Tipo: str("Tamaño 13x18")
+                    print("Digite la cantidad de Fotos que requiere")
+                    Cant = int(input())
+                    Price = Fotos.Fotos13x18(Cant)
+                    print (F"El precio a pagar es: ", Price)
+                else: 
+                    if F == 3:
+                        Tipo: str("Tamaño 20x25")
+                        print("Digite la cantidad de Fotos que requiere")
+                        Cant = int(input())
+                        Price = Fotos.Fotos20x25(Cant)
+                        print (F"El precio a pagar es: ", Price)
+        else: 
+            if Categoria == 2:
+                Cat = str("Servicio Fotografico")
+                print("seleccione una de las siguientes opciones:\n1. 8 Fotos \n2. 12 Fotos \n3. 24 Fotos")
+                F = int(input())
+                if F ==1:
+                    Tipo = str("8 Fotos")
+                    Price = Servicio_Fotografico.Fotos8()
+                    print(f"El precio a pagar es: ", Price)     
 
-        Pendiente = {"nombre": nombre,"Categoria":Cat}
+                else:
+                    if F == 2:
+                        Tipo = str("12 Fotos")
+                        Price = Servicio_Fotografico.Fotos12()
+                        print(f"El precio a pagar es: ", Price)
+                    else:
+                        if F ==3:    
+                            Tipo = str("24 Fotos")
+                            Price = Servicio_Fotografico.Fotos24()
+                            print(f"El precio a pagar es: ", Price)
+            else:
+                if Categoria == 3:
+                    Cat = str("Impresion")
+                    print("seleccione una de las siguientes opciones:\n1. 8 Fotos \n2. 12 Fotos \n3. 24 Fotos")                    
+        Pendiente = {"nombre": nombre,"Categoria":Cat,"Tipo": Tipo, "Cantidad": Cant, "Precio": Price}
 
     def show_Data(self)->None:
         ...     
